780d361f0b446493a9a65e948565a1028ef52c33
guirofeoli <guirfo@gmail.com>
2025-11-30 00:19:30 -0300

    refactor: remove old base44Client.js

--- src/api/base44Client.js
+++
@@ -1,7 +0,0 @@
-// API Client - connects to Cloudflare Pages Functions
-import { apiClient } from './apiClient';
-
-// Export API client as base44 to maintain compatibility
-export const base44 = apiClient;
-
-export default apiClient;
